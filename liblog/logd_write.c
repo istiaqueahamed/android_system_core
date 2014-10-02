7f3e362acff3f8e9d236c5aee52825720b0630d4
Maliha Momtaz Islam <malihaislam81@gmail.com>
2014-10-02 12:37:44 +0600

    Add MediaTek xlog support

--- liblog/logd_write.c
+++ liblog/logd_write.c
@@ -413,3 +413,23 @@ int __android_log_btwrite(int32_t tag, char type, const void *payload,
 
     return write_to_log(LOG_ID_EVENTS, vec, 3);
 }
+
+#ifdef MTK_HARDWARE
+struct xlog_record {
+        const char *tag_str;
+        const char *fmt_str;
+        int prio;
+};
+
+int __xlog_buf_printf(int bufid, const struct xlog_record *rec, ...)
+{
+    va_list ap;
+    char buf[LOG_BUF_SIZE];
+
+    va_start(ap, rec->fmt_str);
+    vsnprintf(buf, LOG_BUF_SIZE, rec->fmt_str, ap);
+    va_end(ap);
+
+    return __android_log_buf_write(bufid, rec->prio, rec->tag_str, buf);
+}
+#endif
